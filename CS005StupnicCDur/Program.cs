554e9affdaf107d6f1c1b7a1f1512520a8c7a981
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:10:43 +0200

    Add files via upload

---
+++ CS005StupnicCDur/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS005StupnicCDur
+{
+    class Program
+    {
+        const float FC = 261.63f;
+        const float FD = 293.66f;
+        const float FE = 329.63f;
+        const float FF = 349.23f;
+        const float FG = 392f;
+        const float FA = 440f;
+        const float FB = 492.88f;
+        const float FC2 = 523.25f;
+
+        static void Main(string[] args)
+        {
+            Console.Beep((int)FC, 100);
+            Console.Beep((int)FD, 100);
+            Console.Beep((int)FE, 100);
+            Console.Beep((int)FF, 100);
+            Console.Beep((int)FG, 100);
+            Console.Beep((int)FA, 100);
+            Console.Beep((int)FB, 100);
+            Console.Beep((int)FC2, 100);
+            Console.Beep((int)FB, 100);
+            Console.Beep((int)FA, 100);
+            Console.Beep((int)FG, 100);
+            Console.Beep((int)FF, 100);
+            Console.Beep((int)FE, 100);
+            Console.Beep((int)FD, 100);
+            Console.Beep((int)FC, 100);
+        }
+    }
+}
